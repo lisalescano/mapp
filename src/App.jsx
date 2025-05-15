acec3f7ab1529c430dda257a2f1baf418bdd43e0
lisalescano <lisalescano10@gmail.com>
2025-05-14 22:26:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'leaflet/dist/leaflet.css'
+import Mapa from './views/Mapas';
+
+function App() {
+    return (
+        <div>
+            <h1>mapa santiago del estero</h1>
+            <Mapa />
+        </div>
+    );
+}
+
+export default App;
